e268601546b48b367924dd8578639ea7cc506b47
= <=>
2025-07-17 21:13:38 -0300

    refazendo o header

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -2,27 +2,30 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-import { MatIconModule } from '@angular/material/icon';
+
 
 @Component({
   selector: 'app-header',
+  standalone: true,
   imports: [
     CommonModule,
     FormsModule,
     RouterModule,
-    MatIconModule
+
   ],
   templateUrl: './header.html',
-  styleUrl: './header.css'
+  styleUrls: ['./header.css']
 })
 export class Header {
-  logado = !!localStorage.getItem('usuario');
+  logado = !!localStorage.getItem('token');
   tipoUsuario = localStorage.getItem('tipoUsuario');
+  usuarioNome = localStorage.getItem('usuarioNome');
+  avatarUrl = localStorage.getItem('avatarUrl');
   mostrarBusca = location.pathname === '/explorar';
   busca = '';
 
   logout() {
     localStorage.clear();
-    location.href = '/login';
+    window.location.href = '/login';
   }
 }
