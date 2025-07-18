6f463b0354b781681872b782ed93f11330e4e7e8
= <=>
2025-07-17 22:04:55 -0300

    salvando o login refeito

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,34 +1,19 @@
-
-import {ChangeDetectionStrategy, Component, signal} from '@angular/core';
-import {MatSelectModule} from '@angular/material/select';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import { Router } from '@angular/router';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import {MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [
-    MatFormFieldModule, 
-    MatInputModule, 
-    MatSelectModule,
-    MatSlideToggleModule,
-    MatButtonModule,
-    MatIconModule, 
-    RouterModule
-  ],
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: true,
+  imports: [CommonModule, FormsModule, RouterModule],
   templateUrl: './login.html',
-  styleUrl: './login.css'
+  styleUrls: ['./login.css']
 })
 export class Login {
-  hide = signal(true);
-  clickEvent(event: MouseEvent) {
-    this.hide.set(!this.hide());
-    event.stopPropagation();
+  hidePassword = true;
+
+  togglePassword() {
+    this.hidePassword = !this.hidePassword;
   }
 }
