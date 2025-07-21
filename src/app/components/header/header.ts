bf60bfb7e7f1f52a0f23db30a8341f8e04c5c47b
= <=>
2025-07-21 16:56:20 -0300

    ajustando o header

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,9 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-
-
+import { MatIconModule } from '@angular/material/icon';
+import {MatExpansionModule} from '@angular/material/expansion';
 @Component({
   selector: 'app-header',
   standalone: true,
@@ -11,18 +11,33 @@ import { RouterModule } from '@angular/router';
     CommonModule,
     FormsModule,
     RouterModule,
-
+    MatIconModule,
+    MatExpansionModule
   ],
   templateUrl: './header.html',
   styleUrls: ['./header.css']
 })
-export class Header {
-  logado = !!localStorage.getItem('token');
-  tipoUsuario = localStorage.getItem('tipoUsuario');
-  usuarioNome = localStorage.getItem('usuarioNome');
-  avatarUrl = localStorage.getItem('avatarUrl');
-  mostrarBusca = location.pathname === '/explorar';
+export class Header implements OnInit {
+  readonly panelOpenState = signal(false);
+  logado = false;
+  tipoUsuario: string | null = null;
+  usuarioNome: string | null = null;
+  avatarUrl: string | null = null;
+  mostrarBusca = false;
   busca = '';
+
+  ngOnInit() {
+    this.atualizarEstado();
+    window.addEventListener('storage', () => this.atualizarEstado()); // Detecta alterações no localStorage em outras abas
+  }
+
+  atualizarEstado() {
+    this.logado = !!localStorage.getItem('token');
+    this.tipoUsuario = localStorage.getItem('tipoUsuario');
+    this.usuarioNome = localStorage.getItem('usuarioNome');
+    this.avatarUrl = localStorage.getItem('avatarUrl');
+    this.mostrarBusca = location.pathname === '/explorar';
+  }
 
   logout() {
     localStorage.clear();
