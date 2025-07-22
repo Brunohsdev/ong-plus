5689ff0e437eb29f50dca613ce5e55a7ae0fa4f2
BRUNO SANTOS <bhas795002@gmail.com>
2025-07-22 11:17:58 -0300

    salvando o header no cadastro

--- src/app/pages/cadastro/cadastro.ts
+++ src/app/pages/cadastro/cadastro.ts
@@ -6,6 +6,7 @@ import { RouterModule } from '@angular/router';
 import { FormOng } from '../../components/form-ong/form-ong';
 import { FormDoador } from '../../components/form-doador/form-doador';
 import { Footer } from '../../components/footer/footer';
+import { Header } from '../../components/header/header';
 
 @Component({
   selector: 'app-cadastro',
@@ -15,6 +16,7 @@ import { Footer } from '../../components/footer/footer';
     RouterModule, 
     FormOng, 
     FormDoador,
+    Header
   Footer],
   templateUrl: './cadastro.html',
   styleUrls: ['./cadastro.css']
